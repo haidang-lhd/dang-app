c3578d3ee6fd950af1f1a1f745a393359167fb4c
dangle <haidang.lhd.lhd@gmail.com>
2023-12-11 21:46:39 +0700

    remove client apis

--- app/views/clients/index.jbuilder
+++
@@ -1,8 +0,0 @@
-json.success true
-json.clients @clients do |client|
-  json.id client.secondary_id
-  json.full_name client.full_name
-  json.email client.email
-  json.first_name client.first_name
-  json.last_name client.last_name
-end
--- spec/factories/clients.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :client do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.safe_email }
-    secondary_id { Faker::Number.number(10) }
-  end
-end
